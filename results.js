55b71d0758cec535ca9f0245537ca66c1dccca56
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2017-03-03 01:15:18 +0000

    Removed glitched tab indent

--- results.js
+++ results.js
@@ -4,8 +4,8 @@ function markresults() {
     results = document.getElementsByTagName("a");
     for (i = 0; i < results.length; i++) {
     	if (wl.indexOf(results[i].hostname) > -1 && !(results[i].hostname == "") && !(results[i].className == "_Fmb ab_button")) {
-    		results[i].style.backgroundColor = "lightgreen";
-    		results[i].style.borderRadius = "25px";
+    	    results[i].style.backgroundColor = "lightgreen";
+    	    results[i].style.borderRadius = "25px";
     	}
     }
 }
