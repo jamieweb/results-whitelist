856f82c64fe1040c773c7a6c6667f2076c72ca38
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2017-03-03 01:16:39 +0000

    Replaced tabs with spaces

--- results.js
+++ results.js
@@ -3,9 +3,9 @@ chrome.storage.sync.get(function(items) { wl = items.wl.split("\n"); });
 function markresults() {
     results = document.getElementsByTagName("a");
     for (i = 0; i < results.length; i++) {
-    	if (wl.indexOf(results[i].hostname) > -1 && !(results[i].hostname == "") && !(results[i].className == "_Fmb ab_button")) {
-    	    results[i].style.backgroundColor = "lightgreen";
-    	    results[i].style.borderRadius = "25px";
+        if (wl.indexOf(results[i].hostname) > -1 && !(results[i].hostname == "") && !(results[i].className == "_Fmb ab_button")) {
+            results[i].style.backgroundColor = "lightgreen";
+            results[i].style.borderRadius = "25px";
     	}
     }
 }
