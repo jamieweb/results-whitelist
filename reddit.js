75900a863756285976bbd0b81b9a938d9e6b1899
Jamie Scaife <JamieOnUbuntu@users.noreply.github.com>
2017-04-04 19:53:40 +0100

    Added support for filtering reddit posts + search results

---
+++ reddit.js
@@ -0,0 +1,13 @@
+chrome.storage.sync.get(function(items) { wl = items.wl.split("\n"); });
+
+function markresults() {
+    results = document.getElementsByTagName("a");
+    for (i = 0; i < results.length; i++) {
+        if (wl.indexOf(results[i].hostname) > -1 && !(results[i].hostname == "") && ((results[i].classList.contains("title")) || (results[i].classList.contains("search-title")))) {
+            results[i].style.backgroundColor = "lightgreen";
+            results[i].style.borderRadius = "25px";
+    	}
+    }
+}
+
+window.addEventListener('load', setTimeout(markresults,1100));
